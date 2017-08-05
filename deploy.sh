f67ab4e890d251b60f9b17717c80416091d9c3c0
Adam CRow <adamcrow63@gmail.com>
2017-08-05 17:02:14 +1000

    Added realm dir

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 MY_IP=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+echo "host ip = ${MY_IP}"
 MY_IP=${MY_IP} docker stack deploy --compose-file swarmtest.yml   --with-registry-auth   swarmtest 
 
