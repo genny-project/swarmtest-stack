f503696072e53e57881a8ecbd4dcdb9f652a7aad
Adam CRow <adamcrow63@gmail.com>
2017-08-05 15:04:28 +1000

    Initial docker swarm deploy

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+MY_IP=`ifconfig eth0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1`
+MY_IP=${MY_IP} docker stack deploy --compose-file swarmtest.yml   --with-registry-auth   swarmtest 
+
